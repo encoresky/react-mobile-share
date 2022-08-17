6c2f3bfd4effd6f9b41e16a4c2aabf165cff83af
Raj Kumar <raj.vishwakarma@encoresky.com>
2022-08-17 16:26:43 +0530

    param list fixes.

--- index.js
+++ index.js
@@ -16,19 +16,31 @@ const dataURLtoFile = (dataurl) => {
   return new File([u8arr], "image.jpg", { type: mime });
 };
 
-exports.shareOnMobile = async function ({ text, url, title, imgBase64 }) {
+exports.shareOnMobile = async function ({ url, title, image }) {
   if (navigator.share === undefined) {
     console.error("error: navigator.share is not available");
     return;
   }
-  var file = dataURLtoFile(imgBase64);
+  if (!title) {
+    console.error("error: title is requied");
+    return;
+  }
+
+  var shareData = { text: "" };
+  if (title) {
+    shareData.title = title;
+  }
+  if (url) {
+    shareData.url = url;
+  }
+  if (image) {
+    var file = dataURLtoFile(image);
+    if (file) {
+      shareData.files = [file];
+    }
+  }
   try {
-    await navigator.share({
-      text,
-      url,
-      title,
-      files: [file]
-    });
+    await navigator.share(shareData);
   } catch (error) {
     console.error("error: ", error);
   }
