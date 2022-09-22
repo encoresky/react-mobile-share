be955b4ce05e7a902e334c43417cdb9a0da09131
Raj Kumar <raj.vishwakarma@encoresky.com>
2022-09-22 20:31:16 +0530

    add fallback and optimizations

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
+const { dataURLtoFile } = require("./url-to-file");
 
-const dataURLtoFile = (dataurl) => {
-  if (!dataurl) {
-    return;
-  }
-  var arr = dataurl.split(","),
-    mime = arr[0].match(/:(.*?);/)[1],
-    bstr = atob(arr[1]),
-    n = bstr.length,
-    u8arr = new Uint8Array(n);
+exports.shareOnMobile = (params, fallback) => {
+  const { url, title, image } = params
 
-  while (n--) {
-    u8arr[n] = bstr.charCodeAt(n);
-  }
-
-  return new File([u8arr], "image.jpg", { type: mime });
-};
-
-exports.shareOnMobile = function ({ url, title, image }) {
   if (navigator.share === undefined) {
+    fallback?.("Can't share on this, make sure you are running on Android or iOS devices")
     console.error("error: navigator.share is not available");
     return;
   }
   if (!title) {
+    fallback?.("Title is required")
     console.error("error: title is requied");
     return;
   }
@@ -39,11 +27,15 @@ exports.shareOnMobile = function ({ url, title, image }) {
   try {
     if (navigator.canShare && navigator.canShare(shareData)) {
       navigator.share(shareData)
-        .then(() => console.info('Share was successful.'))
-        .catch((error) => console.error('Sharing failed', error));
+        .then(() => console.info('Shared successful.'))
+        .catch((error) => {
+          fallback?.(error.message)
+          console.error('Sharing failed', error);
+        });
     }
 
   } catch (error) {
+    fallback?.(error.message)
     console.error("error: ", error);
   }
 };
